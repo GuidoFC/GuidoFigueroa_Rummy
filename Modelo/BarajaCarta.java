c875a8a26de7e8c90bd0ff18fce155cdeeeadf4a
GuidoFC <guidofigueroa96@gmail.com>
2024-05-11 10:22:04 +0200

    Hacemos una prueba de que existe esta rama con un commit and push

--- Modelo/BarajaCarta.java
+++ Modelo/BarajaCarta.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 public class BarajaCarta {
     // Si necesitamos 2 barajas de cartas, vale la pena hacer 2 arrayList
     // o con uno es suficiente? He decidido hacerlo con uno
+
+
+    // Hacemos una prueba de que existe esta rama con un commit and push
     private ArrayList<Carta> listBarajaCartas ;
 
 
