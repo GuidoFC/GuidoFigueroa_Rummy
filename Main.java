c5c5a52c55d59d674cdf872cb89b90ca98994b02
GuidoFC <guidofigueroa96@gmail.com>
2024-06-05 19:10:25 +0200

    La clase FileUtil funciona, pero la tengo que a finar para que me funcione como yo quiero

--- Main.java
+++ Main.java
@@ -1,8 +1,14 @@
 import RummyArgentino.Prueba;
 import RummyKub.Controlador.Juego;
+import RummyKub.Modelo.GuardarPartidaJson;
 import RummyKub.Modelo.Jugador;
+import Util.JsonReader;
+import com.google.gson.Gson;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import static Util.JsonFileWriter.construirRutaArchivoJson;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,8 +19,11 @@ public class Main {
             Jugador player1 =new Jugador("Guido");
             Jugador player2 =new Jugador("Maria");
             Juego juego = new Juego(player1, player2);
-        } else if (opcionElegida == 2) {
+
+        }  else if (opcionElegida == 2) {
             Prueba.pintarArgentino();
+        }  else if (opcionElegida == 3) {
+
         }
 
     }
@@ -23,6 +32,7 @@ public class Main {
                 Bienvenido, a que juego quiere jugar:
                     1) RummyKub
                     2) RummyArgentino
+                    3) Recuperar una Partida
                 """);
     }
 
@@ -33,4 +43,38 @@ public class Main {
         int opcion = sc.nextInt();
         return opcion;
     }
+
+//    private static GuardarPartidaJson leerCopiaSeguridad(ArrayList<Jugador> listaJugadores){
+//        // Tengo que generar el nombre de la ruta para poder acceder a esa copia
+//        String nombresUnindos = "RummyKub_";
+//        for (Jugador jugador: listaJugadores) {
+//            nombresUnindos = nombresUnindos + jugador.getNombre();
+//        }
+//
+//        Gson gson = new Gson();
+//        // Nota: Nombre_Juego lo tengo declarado como una constante (final) para saber que
+//        // como tiene que empezar la ruta
+//        String ubicacionArchivo = construirRutaArchivoJson(NOMBRE_JUEGO ,nombresUnindos);
+//        String partidaGuardada = "";
+//        try {
+//            // Lo que hacemos es coger la ruta donde tengo guardado el archivo JSON
+//            // y luego me guarda toda esa informcion en una cadena de texto (String)
+//            // el posible error es que la ruta este mal, otro error es que el archivo
+//            // que esta leyendo tenga errores de sintaxis, por eso lo metemos dentro de un Try and Catch
+//            partidaGuardada = JsonReader.readFileAsString(ubicacionArchivo);
+//        }catch (Exception e){
+//            System.out.println(e.getMessage());
+//        }
+//
+//        // crear un objeto Gson e invocar a su método fromJson para
+//        // conseguir pasar de Json a un Objeto. En este caso me devolvera
+//        // un objeto de la clase GuardarPartidaJson donde lo podre usar
+//        // para recuperar la partida. Este método lo tengo que poner en el main
+//        // y tendria que hacer un nuevo constructor para introducir las variables
+//        // que he recuperado.
+//        // mirar info de esta pagina https://adictosaltrabajo.com/2012/09/17/gson-java-json/
+//        // apartado: 3.10. Leyendo JSON desde un fichero.
+//        GuardarPartidaJson GuardarPartidaJsonCopia;
+//        return GuardarPartidaJsonCopia = gson.fromJson(partidaGuardada, GuardarPartidaJson.class);
+//    }
 }
---
+++ Util/FileUtil.java
@@ -0,0 +1,61 @@
+package Util;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileUtil {
+
+    private static final String RECURSOS_PATH = "RummyKub/Recursos/";
+
+    /**
+     * Devuelve una lista de nombres de todos los archivos en la carpeta "Recursos".
+     *
+     * @return Lista de nombres de archivos.
+     */
+    public static List<String> listarArchivosEnCarpeta() {
+        List<String> nombresArchivos = new ArrayList<>();
+        File carpeta = new File(RECURSOS_PATH);
+
+        if (carpeta.exists() && carpeta.isDirectory()) {
+            File[] archivos = carpeta.listFiles();
+            if (archivos != null) {
+                for (File archivo : archivos) {
+                    if (archivo.isFile()) {
+                        nombresArchivos.add(archivo.getName());
+                    }
+                }
+            }
+        }
+        return nombresArchivos;
+    }
+
+    /**
+     * Devuelve el contenido de un archivo específico en la carpeta "Recursos".
+     *
+     * @param nombreArchivo Nombre del archivo a leer.
+     * @return Contenido del archivo como String.
+     * @throws IOException Si ocurre un error al leer el archivo.
+     */
+    public static String leerArchivo(String nombreArchivo) throws IOException {
+        String filePath = RECURSOS_PATH + File.separator + nombreArchivo;
+        return new String(Files.readAllBytes(Paths.get(filePath)));
+    }
+
+    public static void main(String[] args) {
+        // Listar los archivos en la carpeta "Recursos"
+        List<String> archivos = listarArchivosEnCarpeta();
+        System.out.println("Archivos en la carpeta 'Recursos': " + archivos);
+
+        // Leer un archivo específico
+        try {
+            String contenido = leerArchivo("partida_GuidoMaria.json");
+            System.out.println("Contenido del archivo: " + contenido);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
